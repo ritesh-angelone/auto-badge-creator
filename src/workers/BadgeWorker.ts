8300d720ea5c0f3b60cf72420bbaefa9b456e2a6
Ritesh Jain <ritesh.2jain@angelbroking.com>
2023-05-03 16:49:23 +0530

    fix(badge-label): fixes badge label to correctly reflect the coverage type

--- src/workers/BadgeWorker.ts
+++ src/workers/BadgeWorker.ts
@@ -6,11 +6,11 @@ export class Badge {
   private static badgeBaseURL = Globals.BADGE_BASE_URL;
 
   public static create(options: BadgeOptionsObjectInterface, stats: BadgeStatsInterface, prefix: string): string {
-    let badgeURL = this.badgeBaseURL + `Coverage-${Math.round(stats.coverage)}${encodeURI('%')}-${stats.color}.svg?`;
+    let badgeURL = this.badgeBaseURL + `${prefix}-${Math.round(stats.coverage)}${encodeURI('%')}-${stats.color}.svg?`;
 
     for (const option of Object.keys(options)) {
       badgeURL = badgeURL.concat(`${option}=${options[option]}&`);
     }
-    return badgeURL.concat(`prefix=$${prefix}$)`);
+    return badgeURL;
   }
 }
